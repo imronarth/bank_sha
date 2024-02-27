012570192695683d76aa1b42717d6323cea1ecfe
imronarth <imron@gmx.com>
2024-02-27 10:17:46 +0700

    app bank_sha dari buildwithangga

---
+++ lib/shared/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor            = const Color(0XffFFFFFF);
+Color blackColor            = const Color(0Xff14193F);
+Color greyColor             = const Color(0XffA4A8AE);
+Color lightBackgroundColor  = const Color(0XffF6F8FB);
+Color blackBackgroundColor  = const Color(0Xff020518);
+Color blueColor             = const Color(0Xff53C1F9);
+Color purpleColor           = const Color(0Xff5142E6);
+Color greenColor            = const Color(0Xff22B07D);
+Color numberBackgroundColor = const Color(0Xff1A1D2E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor 
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor
+);
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: blueColor
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greenColor
+);
+
+FontWeight light     = FontWeight.w300;
+FontWeight regular   = FontWeight.w400;
+FontWeight medium    = FontWeight.w500;
+FontWeight semiBold  = FontWeight.w600;
+FontWeight bold      = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black     = FontWeight.w900;
