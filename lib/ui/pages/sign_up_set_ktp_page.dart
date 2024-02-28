7911588420de0e686b0b7912b112c1db2ffd02d4
imronarth <imron@gmx.com>
2024-02-28 11:28:08 +0700

    create set ktp sign up

--- lib/ui/pages/sign_up_set_ktp_page.dart
+++ lib/ui/pages/sign_up_set_ktp_page.dart
@@ -1,10 +1,9 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
-import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
 
-class SignUpSetProfilePage extends StatelessWidget {
-  const SignUpSetProfilePage({super.key});
+class SignUpSetKtpPage extends StatelessWidget {
+  const SignUpSetKtpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -44,62 +43,60 @@ class SignUpSetProfilePage extends StatelessWidget {
             ),
             child: Column(
               children: [
-                // Container(
-                //   width: 120,
-                //   height: 120,
-                //   decoration: BoxDecoration(
-                //     shape: BoxShape.circle,
-                //     color: lightBackgroundColor,
-                //   ),
-                //   child: Center(
-                //     child: Image.asset(
-                //       'assets/ic_upload.png',
-                //       width: 32,
-                //     ),
-                //   ),
-                // ),
                 Container(
                   width: 120,
                   height: 120,
-                  decoration: const BoxDecoration(
+                  decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        "assets/img_profile.png"
-                      )
+                    color: lightBackgroundColor,
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/ic_upload.png',
+                      width: 32,
                     ),
                   ),
                 ),
+                // Container(
+                //   width: 120,
+                //   height: 120,
+                //   decoration: const BoxDecoration(
+                //     shape: BoxShape.circle,
+                //     image: DecorationImage(
+                //       fit: BoxFit.cover,
+                //       image: AssetImage(
+                //         "assets/img_profile.png"
+                //       )
+                //     ),
+                //   ),
+                // ),
                 const SizedBox(
                   height: 16,
                 ),
                 Text(
-                  "Imron Athoriq",
+                  "Passport/ID Card",
                   style: blackTextStyle.copyWith(
                     fontSize: 18,
                     fontWeight: medium,
                   ),
                 ),
                 const SizedBox(
-                  height: 30,
-                ),
-                const CustomFormField(
-                  title: "Set PIN (6 digit number)",
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 30,
+                  height: 50,
                 ),
                 CustomFilledButton(
                   title: "Continue",
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/sign-up-set-ktp");
-                  },
-                )
+                  onPressed: () {},
+                ),
               ],
             ),
           ),
+          const SizedBox(
+            height: 60,
+          ),
+          CustomTextButton(
+            title: "Skip for Now",
+            onPressed: () {},
+          )
         ],
       ),
     );
