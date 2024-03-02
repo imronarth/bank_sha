54536719e5c211376874d9a1724c3a69340769d1
imronarth <imron@gmx.com>
2024-03-03 00:27:00 +0700

    create edit page

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:bank_sha/ui/pages/home_page.dart';
 import 'package:bank_sha/ui/pages/onboarding_page.dart';
 import 'package:bank_sha/ui/pages/pin_page.dart';
 import 'package:bank_sha/ui/pages/profil_page.dart';
-import 'package:bank_sha/ui/pages/profile_edit.dart';
+import 'package:bank_sha/ui/pages/profile_edit_page.dart';
 import 'package:bank_sha/ui/pages/sign_in_page.dart';
 import 'package:bank_sha/ui/pages/sign_up_page.dart';
 import 'package:bank_sha/ui/pages/sign_up_set_ktp_page.dart';
