e42f3954984f30f1d32be9bab09508ee46222d16
imronarth <imron@gmx.com>
2024-03-03 10:08:04 +0700

    created shared value, auth bloc, and auth service

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/pages/home_page.dart';
 import 'package:bank_sha/ui/pages/onboarding_page.dart';
@@ -13,6 +14,7 @@ import 'package:bank_sha/ui/pages/sign_up_set_profile_page.dart';
 import 'package:bank_sha/ui/pages/sign_up_success.dart';
 import 'package:bank_sha/ui/pages/splash_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -21,36 +23,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: lightBackgroundColor,
-        appBarTheme: AppBarTheme(
-          centerTitle: true,
-          backgroundColor: lightBackgroundColor,
-          elevation: 0,
-          iconTheme: IconThemeData(color: blackColor),
-          titleTextStyle: blackTextStyle.copyWith(
-            fontSize: 20, 
-            fontWeight: semiBold
-          ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(),
         )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: lightBackgroundColor,
+          appBarTheme: AppBarTheme(
+            centerTitle: true,
+            backgroundColor: lightBackgroundColor,
+            elevation: 0,
+            iconTheme: IconThemeData(color: blackColor),
+            titleTextStyle: blackTextStyle.copyWith(
+              fontSize: 20, 
+              fontWeight: semiBold
+            ),
+          )
+        ),
+        routes: {
+          '/'                    : (context) => const SplashPage(),
+          '/onboarding'          : (context) => const OnboardingPage(),
+          '/sign-in'             : (context) => const SignInPage(),
+          '/sign-up'             : (context) => const SignUpPage(),
+          '/sign-up-set-profile' : (context) => const SignUpSetProfilePage(),
+          '/sign-up-set-ktp'     : (context) => const SignUpSetKtpPage(),
+          '/sign-up-success'     : (context) => const SignUpSuccessPage(),
+          '/home-page'           : (context) => const HomePage(),
+          '/profile'             : (context) => const ProfilPage(),
+          '/profile-edit'        : (context) => const ProfileEditPage(),
+          '/pin'                 : (context) => const PinPage(),
+          '/profile-edit-pin'    : (context) => const ProfileEditPinPage(),
+          '/profile-edit-success': (context) => const ProfileEditSuccessPage(),
+        },
       ),
-      routes: {
-        '/'                    : (context) => const SplashPage(),
-        '/onboarding'          : (context) => const OnboardingPage(),
-        '/sign-in'             : (context) => const SignInPage(),
-        '/sign-up'             : (context) => const SignUpPage(),
-        '/sign-up-set-profile' : (context) => const SignUpSetProfilePage(),
-        '/sign-up-set-ktp'     : (context) => const SignUpSetKtpPage(),
-        '/sign-up-success'     : (context) => const SignUpSuccessPage(),
-        '/home-page'           : (context) => const HomePage(),
-        '/profile'             : (context) => const ProfilPage(),
-        '/profile-edit'        : (context) => const ProfileEditPage(),
-        '/pin'                 : (context) => const PinPage(),
-        '/profile-edit-pin'    : (context) => const ProfileEditPinPage(),
-        '/profile-edit-success': (context) => const ProfileEditSuccessPage(),
-      },
     );
   }
 }
