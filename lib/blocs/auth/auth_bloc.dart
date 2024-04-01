f610301ef38e32db06d45f1b994ee7032b3afc6e
imronarth <imron@gmx.com>
2024-04-01 14:37:58 +0700

    menambahkan logout

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -107,6 +107,18 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           AuthFailed(e.toString());
         }
       }
+
+      if (event is AuthLogout) {
+        try {
+          emit(AuthLoading());
+
+          await AuthService().logout();
+
+          emit(AuthInitial());
+        } catch (e) {
+          emit(AuthFailed(e.toString()));
+        }
+      }
     });
   }
 }
