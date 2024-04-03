d54b1e118efa1133e8fbef35bb3d317ca6924c68
imronarth <imron@gmx.com>
2024-04-03 11:35:29 +0700

    page package

--- lib/ui/pages/data_provider_page.dart
+++ lib/ui/pages/data_provider_page.dart
@@ -93,7 +93,9 @@ class DataProviderPage extends StatelessWidget {
           ),
           CustomFilledButton(
             title: 'Continue',
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pushNamed(context, '/data-package');
+            },
           ),
           const SizedBox(
             height: 57,
